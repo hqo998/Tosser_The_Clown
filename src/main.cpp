0fd451142b900363b17b7a63b8bbb1d19c72b335
Charles <charlesvonkalm@gmail.com>
2026-01-03 20:59:56 +1100

    add template

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,14 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "Test Window");
+    InitWindow(screenWidth, screenHeight, "Tosser the Clown");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Hello World!", screenWidth/2, screenHeight/2, 20, LIGHTGRAY);
+            DrawText("Tosser the Clown!", screenWidth/2, screenHeight/2, 20, LIGHTGRAY);
         EndDrawing();
     }
 
