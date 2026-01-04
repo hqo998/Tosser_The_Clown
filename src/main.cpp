a84770197d6ff21729ccd088be63f35e15f18021
Charles <charlesvonkalm@gmail.com>
2026-01-04 12:45:46 +1100

    Getting shapes on screen, rendering at 720p then using a render target centred to the screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,81 @@
 #include "raylib.h"
+#include <cmath>
 
-int main(void)
+int main()
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    int screenWidth{500};
+    int screenHeight{500};
+
+    const int gameWidth = 2560;    // Internal Render (720p
+    const int gameHeight = 1440;
+
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
     InitWindow(screenWidth, screenHeight, "Tosser the Clown");
-    SetTargetFPS(60);
+    // ToggleBorderlessWindowed();
+    SetTargetFPS(30);
+
+
+    // load image
+    Texture2D texture = LoadTexture("resources/tmp/coluredCircles.png");
+
+    // base width
+    const float baseWidth = 500.0f;
+
+    RenderTexture2D target = LoadRenderTexture(gameWidth, gameHeight);
+    SetTextureFilter(target.texture, TEXTURE_FILTER_ANISOTROPIC_8X); // Smooth scaling
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
+        screenWidth = GetScreenWidth();
+        screenHeight = GetScreenHeight();
+
+        float scale = (float)gameWidth / baseWidth;
+
+        Rectangle sourceRec = { 0.0f, 0.0f, (float)texture.width, (float)texture.height };
+
+        Rectangle destRec = {
+            (float)gameWidth / 2.0f,
+            (float)gameHeight / 2.0f,
+            (float)texture.width * scale * .1f,
+            (float)texture.height * scale * .1f
+        };
+
+        Vector2 origin = {
+            (float)destRec.width * (float).5,
+            (float)destRec.height * (float).5
+        };
+
+        BeginTextureMode(target);
             ClearBackground(RAYWHITE);
-            DrawText("Tosser the Clown!", screenWidth/2, screenHeight/2, 20, LIGHTGRAY);
+
+            DrawTexturePro(texture, sourceRec, destRec, origin, 0.0f, WHITE);
+
+        EndTextureMode();
+
+        BeginDrawing();
+            ClearBackground(BLACK); // Background if aspect ratios don't match
+
+            // Define source (720p) and destination rectangles with offset to make sure the target is always centred.
+            Rectangle sourceRecTarget = { 0.0f, 0.0f, (float)target.texture.width, (float)-(target.texture.height) };
+
+            float widthScaleTarget =  (float)screenHeight / target.texture.height;
+            float centredWidth = (float)target.texture.width * widthScaleTarget;
+            float centredOriginOffset = ((centredWidth - float(screenWidth))/2);
+
+            Rectangle destRecTarget = { 0.0f, 0.0f, centredWidth, (float)screenHeight };
+            Vector2 originTarget = { 0 + centredOriginOffset, 0 };
+
+            DrawTexturePro(target.texture, sourceRecTarget, destRecTarget, originTarget, 0.0f, WHITE);
+
+            DrawText(TextFormat("Offset: %f", centredOriginOffset), 0, 40, 20, GREEN);
+            DrawFPS(10, 10);
         EndDrawing();
     }
 
+    UnloadRenderTexture(target);
+    UnloadTexture(texture);
     CloseWindow();
 
     return 0;
