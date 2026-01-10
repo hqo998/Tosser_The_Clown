6fc08e1a43434d5234a8115308fccd753c66061a
Charles <charlesvonkalm@gmail.com>
2026-01-10 16:10:19 +1100

    clown animation structs

--- src/Clown/Clown.cpp
+++ src/Clown/Clown.cpp
@@ -1,5 +1,13 @@
 #include "Clown.hpp"
 
+// talks on main menu screen
+
+// when playing clicks and drags does throwing idle
+
+// on release does throw animation
+
+// get hit animation?
+
 Clown::Clown()
 {
     sprite.LoadSprite("resources/tmp/TosserSpritesheetTest.png", 9, 2);
--- src/Clown/Clown.hpp
+++ src/Clown/Clown.hpp
@@ -3,6 +3,27 @@
 #include "../AnimationPlayer/AnimationPlayer.hpp"
 #include "../Sprite/Sprite.hpp"
 
+#pragma region CLOWN_ANIMATIONS
+AnimationSequence clownThrow = {
+        "Throw",
+        {0, 0}, // start frame
+        9,      // how many frames long
+        5,      // fps speed
+        false,  // do loop
+        {},     // what frames to pause on and how many frames for
+    };
+
+AnimationSequence clownTalk = {
+        "Talk",
+        {0, 1}, // start frame
+        2,      // how many frames long
+        2,      // fps speed
+        true,   // do loop
+        {},     // what frames to pause on and how many frames for
+    };
+#pragma endregion
+
+
 class Clown
 {
     AnimationPlayer AniPlayer;
